7beaa6427ff056d60fbebcd1b5bcf2d4e0a25b25
Suzigan1 <guilhermesuzigan@hotmail.com>
2020-05-25 02:42:38 -0300

    First Commit

---
+++ GameTOP.Lib/Jogador3.cs
@@ -0,0 +1,20 @@
+namespace GameTOP.Lib
+{
+    public class Jogador3 : iJogador
+    {
+        public string Chuta()
+        {
+            return "Teste Chuta \n";
+        }
+
+        public string Corre()
+        {
+            return"Teste Corre \n";
+        }
+
+        public string Passe()
+        {
+              return "Teste Passe \n";
+        }
+    }
+}
